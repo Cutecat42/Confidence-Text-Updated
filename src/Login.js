2d88209101794bc20553d82e2b70bc92faa35da2
Cat Meza <musical_kitty@hotmail.com>
2021-04-21 13:24:23 -0600

    create functioning login button with auth0

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import LoginButton from './LoginButton';
 
 const Login = () => {
     return (
         <div>
             Hi
+            <LoginButton />
         </div>
     )
 }
