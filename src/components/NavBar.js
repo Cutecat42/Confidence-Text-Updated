ac33d3a475a9143af94e31159d09e1f708b995b6
Cat Meza <musical_kitty@hotmail.com>
2021-04-23 15:45:28 -0600

    add navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,38 @@
-import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink }from 'react-router-dom';
+import  Nav from 'react-bootstrap/Nav';
 import React from 'react';
-import '../css/NavBar.css';
 
 const NavBar = ({user}) => {
     if (!user) return (
-        <>
-        <nav className='MyNavBarClass'>
-            <NavLink exact to='/' className='navBarLink'>
-                Home 
-            </NavLink>
-            <NavLink exact to='/login' className='navBarLink'>
-                Login 
-            </NavLink> 
-        </nav>
-        </>
+        <Navbar bg="dark" variant="dark">
+            <NavLink exact to='/' className='navbar-brand h4'>Confidence Text</NavLink>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                <Nav>
+                    <NavLink exact to='/login' className='text-decoration-none text-light'>Login</NavLink>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 
     return (
-        <>
-        <nav className='MyNavBarClass'>
-            <NavLink exact to='/daily' className='navBarLink'>
-                Daily Inspirational Quote
-            </NavLink>
-            <NavLink exact to='/profile' className='navBarLink'>
-                Profile
-            </NavLink>
-            <NavLink exact to='/logout' className='navBarLink'>
-                Logout
-            </NavLink>
-        </nav>
-        </>
-
+        <Navbar bg="dark" variant="dark">
+            <NavLink exact to='/daily' className='navbar-brand h4'>Confidence Text</NavLink>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+                <NavLink exact to='/daily' className='text-decoration-none text-light mr-3'>Daily Inspirational Quote</NavLink>
+            </Nav>
+            <Nav>
+                <NavLink exact to='/profile' className='text-decoration-none text-light mr-3'>Profile</NavLink>
+            </Nav>
+            <Nav>
+                <NavLink exact to='/logout' className='text-decoration-none text-light mr-3'>Logout</NavLink>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
     )
 };
 
