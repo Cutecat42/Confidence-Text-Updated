700251e7845eec1c72ea2e64f9c58f7f87c7bbb3
Cat Meza <musical_kitty@hotmail.com>
2021-04-24 12:17:36 -0600

    fix button to go back and update some css

--- src/components/AllQuotes.js
+++ src/components/AllQuotes.js
@@ -1,15 +1,19 @@
 import ConfidenceTextsAll from '../database/ConfidenceTextsAll';
-import  Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 const AllQuotes = () => {
+
     return (
         <div>
             <br></br>
-            <p>Be nice to yourself and say these every day!</p>
+            <h4>Be nice to yourself and say these every day!</h4>
+            <br></br>
             <ConfidenceTextsAll />
             <br></br>
-            <Button className='btn btn-secondary'>Go back</Button>
+            <Link className='btn btn-secondary' role='button' to='/daily'> 
+                Go back
+            </Link>
             <br></br>
             <br></br>
         </div>
